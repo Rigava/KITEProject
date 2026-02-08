4dfbe24cdbb3bf925bd67793d5adda3de0ed34dc
NetZero <73232007+Rigava@users.noreply.github.com>
2026-02-08 21:18:16 +0530

    Update st_app.py

--- st_app.py
+++ st_app.py
@@ -77,29 +77,30 @@ if st.button("Fetch Data"):
         st.error("No data received. Check token, dates, or interval.")
 
 if st.button("Shortlist RSI"):
-    tickers = pd.read_html('https://ournifty.com/stock-list-in-nse-fo-futures-and-options.html#:~:text=NSE%20F%26O%20Stock%20List%3A%20%20%20%20SL,%20%201000%20%2052%20more%20rows%20')[0]
-    symbol_list = tickers.SYMBOL.to_list()
-    symbol_list= symbol_list[5:]
-    symbol_list.remove("TATAMOTORS")
-    #SHORTLIST
-    Buy = []
-    Sell = []
-    Hold = []
-    rsi_low = st.sidebar.slider("RSI low for buy", min_value=1, max_value=100, value=30, step=1)
-    rsi_high = st.sidebar.slider("RSI high for sell", min_value=1, max_value=100, value=70, step=1) 
-    for stock in symbol_list:
-        from_date, to_date = enforce_kite_limits(interval, from_date, to_date)
-        df = get_historical_data(enctoken, symbol, interval, from_date, to_date)
-        df = add_indicators(df)
-        if df["RSI"].iloc[-1] > rsi_low and df["RSI"].iloc[-2] < rsi_low: 
-            Buy.append(stock)
-        elif df["RSI"].iloc[-1] < rsi_high and df["RSI"].iloc[-2] > rsi_high:
-            Sell.append(stock)
-        else:
-            Hold.append(stock)  
-        # Display stock data and recommendation
-    st.write(":blue[List of stock with buy signal]",Buy)
-    st.write(":blue[List of stock with sell signal]",Sell)
+    tickers = symbol
+    st.write(tickers)
+    # symbol_list = tickers.SYMBOL.to_list()
+    # symbol_list= symbol_list[5:]
+    # symbol_list.remove("TATAMOTORS")
+    # #SHORTLIST
+    # Buy = []
+    # Sell = []
+    # Hold = []
+    # rsi_low = st.sidebar.slider("RSI low for buy", min_value=1, max_value=100, value=30, step=1)
+    # rsi_high = st.sidebar.slider("RSI high for sell", min_value=1, max_value=100, value=70, step=1) 
+    # for stock in symbol_list:
+    #     from_date, to_date = enforce_kite_limits(interval, from_date, to_date)
+    #     df = get_historical_data(enctoken, symbol, interval, from_date, to_date)
+    #     df = add_indicators(df)
+    #     if df["RSI"].iloc[-1] > rsi_low and df["RSI"].iloc[-2] < rsi_low: 
+    #         Buy.append(stock)
+    #     elif df["RSI"].iloc[-1] < rsi_high and df["RSI"].iloc[-2] > rsi_high:
+    #         Sell.append(stock)
+    #     else:
+    #         Hold.append(stock)  
+    #     # Display stock data and recommendation
+    # st.write(":blue[List of stock with buy signal]",Buy)
+    # st.write(":blue[List of stock with sell signal]",Sell)
     
 if st.button("Backtesting Mean reversion"):  
     from_date, to_date = enforce_kite_limits(interval, from_date, to_date)
